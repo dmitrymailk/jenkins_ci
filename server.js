3f1895c0177da5a76caa21b2607b49025635a166
dmitrymailk <dmitrymail_k@mail.ru>
2022-07-30 02:07:34 +0300

    add commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World and Jenkins!");
+  res.end("Hello World and Jenkins 123!");
 });
 
 server.listen(port, hostname, () => {
